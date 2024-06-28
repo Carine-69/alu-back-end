3a207f0a70b095d3d4289b1050a9ed917c64b222
Carine-69 <umugabecarine@gmail.com>
2024-06-28 11:32:10 +0000

    task 1

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,33 +1,38 @@
 #!/usr/bin/python3
 """Script that gets user data (Todo list) from API
-and then export the result to csv file. """
+and then exports the result to a JSON file."""
 
 import json
 import requests
 
 
 def main():
-    """main function"""
+    """Main function"""
     todo_url = 'https://jsonplaceholder.typicode.com/todos'
 
     response = requests.get(todo_url)
+    if response.status_code != 200:
+        print("Error: Unable to fetch data from the API")
+        return
 
     output = {}
 
     for todo in response.json():
         user_id = todo.get('userId')
-        if user_id not in output.keys():
+        if user_id not in output:
             output[user_id] = []
-            user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(
-                user_id)
-            user_name = requests.get(user_url).json().get('username')
-
-        output[user_id].append(
-            {
-                "username": user_name,
-                "task": todo.get('title'),
-                "completed": todo.get('completed')
-            })
+            user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+            user_name_response = requests.get(user_url)
+            if user_name_response.status_code != 200:
+                print("Error: Unable to fetch user data from the API")
+                return
+            user_name = user_name_response.json().get('username')
+        
+        output[user_id].append({
+            "username": user_name,
+            "task": todo.get('title'),
+            "completed": todo.get('completed')
+        })
 
     with open("todo_all_employees.json", 'w') as file:
         json.dump(output, file)
